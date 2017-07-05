96c18fc6375e90ca24e58717ac85d3735679dbba
rebeccaseel <rseelgws@gmail.com>
2017-07-05 11:56:37 -0400

    continued working on practice problems

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -52,6 +52,40 @@ namespace ArraysPractice
             //loops are best way to print an array --- getting to this on Thursday 7.6.2017
 
 
+            //new keyword = new datatype must be the same datatype as the original
+            //datatype[ nameOfArray = new dataType[#ofElements};
+            //int[] numberOfSeats = new int[21];
+            //string[] studentsInClass = new string[21]; //creating a dresser with 21 drawers, not a value of 21 so string ok
+
+            ////nameOfArray[index] = Element;
+            //studentsInClass[0] = "Jordan";
+            ////allow user to enter the next student
+            //Console.WriteLine("Please enter in the name of the next student.");
+            //studentsInClass[1] = Console.ReadLine();
+            ////to check it was done correctly
+            //Console.WriteLine("The student in the second seat is {0}.", studentsInClass[1]);
+            ////get age of each student
+            //Console.WriteLine("Please enter the age of the person in the first seat.");
+            ////create a new int array with 21 spots and call it ageOfStudentsInClass
+            //int[] ageOfStudentsInClass = new int[21]; 
+            ////Assign the value of the int at the 0 index of ageOfStudentsInClass to...
+            ////...the users's input converted into an int
+            ////console.readline takes info in as string, int.parse converts from string to int
+            //ageOfStudentsInClass[0] = int.Parse(Console.ReadLine());
+            //Console.WriteLine("The age of the student in the first seat is " + ageOfStudentsInClass[0]);
+            ////Create a new string variable and set it equal to the int at the index of 0...
+            ////and convert it into string
+            ////converts int to string -- To.String method builds strings
+            //string firstIndex = ageOfStudentsInClass[0].ToString();
+
+
+
+
+
+             
+
+
+
 
 
 
