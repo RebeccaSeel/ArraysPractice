ff1116c1f2938a0e85a4e7266e8e515147fce496
rebeccaseel <rseelgws@gmail.com>
2017-07-05 11:09:19 -0400

    update 1

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -10,6 +10,58 @@ namespace ArraysPractice
     {
         static void Main(string[] args)
         {
+            ////declaring an array without initializing
+            //int[] myFirstArray;
+            ////declaring and initializing a second array
+            //string[] myStringArray = { "First", "second", "third", "fourth" };
+            ////declare and initialize a char array with the letters of your last name - single quotes for char
+            //char[] myLastName = { 's', 'e', 'e', 'l' };
+            ////I want my console to print three elements of my array using index position
+            //Console.WriteLine(myLastName[0]);
+            //Console.WriteLine(myLastName[3]);
+            //Console.WriteLine(myLastName[1]);
+            ////create the following arrays and print using the correct index numbers:
+            ////Array of Top 10 Vacaction spots;
+            ////print the first and last vacation spots
+            ////say string array instead of string brackets
+            //string[] VacationSpots = { "Paris", "Melbourne", "Sidney", "Orlando", "Vegas", "NY", "Nashville", "Hinton", "Cleveland", "NC" };
+            //Console.WriteLine(VacationSpots[0]);
+            //Console.WriteLine(VacationSpots[9]);
+            ////Array of the ages of 3 children you know
+            ////print each child's age
+            ////concatenation adds ages together instead of reading as string
+            //int[] myChildAge = { 5, 16, 6 };
+            //Console.WriteLine(myChildAge[0]);
+            //Console.WriteLine(myChildAge[1]);
+            //Console.WriteLine(myChildAge[2]);
+            ////Array of GPA's on a 4.0 of scale of 8 students - you don't have to print
+            ////double[] myGPA = ["2.56", "3.70", "2.91", "1.75", "4.00", "2.25", "1.50", "3.42"];
+            ////Array of the first letter of 4 of your classmate's names sitting near you
+            ////print all four on the same line
+            ////Console.WriteLine(myClassmateName); this only works for char, not strings, etc.
+            //char[] myClassmateName = { 'd', 'j', 't', 'j' };
+            //Console.Write(myClassmateName[0]);
+            //Console.Write(myClassmateName[1]);
+            //Console.Write(myClassmateName[2]);
+            //Console.Write(myClassmateName[3]);
+            //Console.WriteLine("");
+            //Console.WriteLine(myClassmateName);
+            //Console.WriteLine("");
+            //Console.WriteLine(myClassmateName[0] + " " + myClassmateName[1] + " " + myClassmateName[2] + " " + myClassmateName[3]);
+            //Console.WriteLine("{0},{1},{2},{3}", myClassmateName[0], myClassmateName[1], myClassmateName[2], myClassmateName[3]);
+            //loops are best way to print an array --- getting to this on Thursday 7.6.2017
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
